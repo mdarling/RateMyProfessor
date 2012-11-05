f9c76aa13eb58ce0e23e789db1c6102b600e1f1b
Kevin Pedretti <kevin.pedretti@gmail.com>
2012-11-05 15:04:36 -0700

    initial commit after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RateMyProfessor::Application.config.secret_token = 'fc0427284ab0d251f565ecabbaabcd1683a4e8b99877aedefd7558a56bfb3872b37f3b49ec520f803ae332ea98b53b5746fc8657446276d7a0a271c4787dcea1'
