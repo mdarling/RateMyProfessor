892992006b27d17286f4aa6fe880c1cb6d37a5d3
Dennis Magee <dmmagee@unm.edu>
2012-12-02 22:16:21 -0700

    Merge changes

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20121202192531) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.integer  "department_id"
+    t.string   "email"
   end
 
   create_table "questions", :force => true do |t|
