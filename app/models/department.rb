d361fa498ff56c1cdd1f6a5c63b4a47f8f6f0ad3
mdarling <amrit914@gmail.com>
2012-12-03 20:29:30 -0700

    Update app/models/department.rb

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,13 +1,13 @@
 class Department < ActiveRecord::Base
-<<<<<<< HEAD
+
   attr_accessible :name
-=======
+
   attr_accessible :name, :shortname
 
->>>>>>> upstream/master
+
   has_many :professors
   has_many :courses
   
-  :validates :name, presence: true
+   validates :name, presence: true
 
 end
