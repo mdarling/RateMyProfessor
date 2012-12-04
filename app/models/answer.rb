a27f5789871e60ac57d0219e27df90df48dab512
Kevin Pedretti <kevin.pedretti@gmail.com>
2012-12-03 22:39:45 -0700

    Fixup models so that seeds.db works. Answers do not need to be unique... e.g., yes/no can be used by multipel questions. Course IDs are allocated automatically and do not need to be validated.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,4 @@ class Answer < ActiveRecord::Base
   attr_accessible :content, :question_id
   belongs_to :question
   validates :content, :question_id, presence: true
-  validates :content, :question_id, uniqueness: true
 end
