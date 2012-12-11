0db7e69105eda55e3e41fb686d4b04ae84510562
Michael Darling <mdarling@ece.unm.edu>
2012-12-11 14:32:00 -0700

    Vicente Seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+# Mayor.create(name: 'Emanuel', city: cities.first)
 
 
 departments = [
@@ -18,7 +18,6 @@ department_refs = {}
 
 
 professors = [
-<<<<<<< HEAD
     {name: 'Abdallah, Chaouki', department: 'ECE' },
     {name: 'Balakrishnan, Ganesh', department: 'ECE' },
     {name: 'Brueck, Steven', department: 'ECE' },
@@ -69,140 +68,43 @@ professors = [
     {name: 'Stefanovic, Darko', department: 'CS' },
     {name: 'Tapia, Lydia', department: 'CS' },
     {name: 'Williams, Lance', department: 'CS' },
-=======
-    {name: 'Chaouki T. Abdallah', department: 'ECE' },
-    {name: 'Ganesh Balakrishnan', department: 'ECE' },
-    {name: 'Steven R. J. Brueck', department: 'ECE' },
-    {name: 'Vincent D. Calhoun', department: 'ECE' },
-    {name: 'Thomas P. Caudell', department: 'ECE' },
-    {name: 'Christos G. Christodoulou', department: 'ECE' },
-    {name: 'Rafael Fierro', department: 'ECE' },
-    {name: 'Charles B. Fleddermann', department: 'ECE' },
-    {name: 'Daniel Feezell', department: 'ECE' },
-    {name: 'Nasir Ghani', department: 'ECE' },
-    {name: 'Mark A. Gilmore', department: 'ECE' },
-    {name: 'Edward D. Graham, Jr.', department: 'ECE' },
-    {name: 'Majeed M. Hayat', department: 'ECE' },
-    {name: 'Gregory L. Heileman', department: 'ECE' },
-    {name: 'Mani Hossein-Zadeh', department: 'ECE' },
-    {name: 'Ravinder K. Jain', department: 'ECE' },
-    {name: 'Sudharman K. Jayaweera', department: 'ECE' },
-    {name: 'Ramiro Jordan', department: 'ECE' },
-    {name: 'Sanjay Krishna', department: 'ECE' },
-    {name: 'Olga Lavrova', department: 'ECE' },
-    {name: 'Daryl O. Lee', department: 'ECE' },
-    {name: 'Luke F. Lester', department: 'ECE' },
-    {name: 'Meeko Oishi', department: 'ECE' },
-    {name: 'Marek Osinski', department: 'ECE' },
-    {name: 'Marios S. Pattichis', department: 'ECE' },
-    {name: 'Fernando Perez-Gonzalez', department: 'ECE' },
-    {name: 'James F. Plusquellic', department: 'ECE' },
-    {name: 'L. Howard Pollard', department: 'ECE' },
-    {name: 'Balu Santhanam', department: 'ECE' },
-    {name: 'Edl Schamiloglu', department: 'ECE' },
-    {name: 'Wei Wennie Shu', department: 'ECE' },
-    {name: 'Payman Zarkesh-Ha', department: 'ECE' },
 
-    {name: 'David Ackley', department: 'CS' },
-    {name: 'Dorian Arnold', department: 'CS' },
-    {name: 'Patrick Bridges', department: 'CS' },
-    {name: 'Jed Crandall', department: 'CS' },
-    {name: 'Stephanie Forrest', department: 'CS' },
-    {name: 'Tom Hayes', department: 'CS' },
-    {name: 'Deepak Kapur', department: 'CS' },
-    {name: 'Patrick Kelley', department: 'CS' },
-    {name: 'Joe Kniss', department: 'CS' },
-    {name: 'Shuang (Sean) Luan', department: 'CS' },
-    {name: 'George F. Luger', department: 'CS' },
-    {name: 'Melanie Moses', department: 'CS' },
-    {name: 'Gruia-Catalin Roman', department: 'CS' },
-    {name: 'Jared Saia', department: 'CS' },
-    {name: 'Darko Stefanovic', department: 'CS' },
-    {name: 'Lydia Tapia', department: 'CS' },
-    {name: 'Lance R. Williams', department: 'CS' },
     
-    {name: 'Nader Ebrahimi', department: 'ME' },
-    {name: 'Robert H. Greenlee', department: 'ME' },
-    {name: 'Chris Hall', department: 'ME' },
-	{name: 'Juan C. Heinrich', department: 'ME' },
-	{name: 'Tariq Khraishi', department: 'ME' },
-	{name: 'Zayd Leseman', department: 'ME' },
-	{name: 'Ron Lumia', department: 'ME' },
-	{name: 'Andrea Mammoli', department: 'ME' },
-	{name: 'Svetlana Poroseva', department: 'ME' },
-	{name: 'Arsalan Razani', department: 'ME' },
-	{name: 'John Russell', department: 'ME' },
-	{name: 'Yu-Lin Shen', department: 'ME' },
-	{name: 'Francesco Sorrentino', department: 'ME' },
-	{name: 'Gregory P. Starr', department: 'ME' },
-	{name: 'C. Randall Truman', department: 'ME' },
-	{name: 'Peter Vorobieff', department: 'ME' },
-	{name: 'John E. Wood', department: 'ME' },
-	{name: 'Alan Graham', department: 'ME' },
-	{name: 'Ihab El-Kady', department: 'ME' },
-	{name: 'Matthias Pleil', department: 'ME' },
-	{name: 'William E. Baker', department: 'ME' },
-	{name: 'James Leith', department: 'ME' },
-	{name: 'Charles G. Richards', department: 'ME' },
-	{name: 'Howard L. Schreyer', department: 'ME' },
-	{name: 'Maurice W. Wildin', department: 'ME' },
-	
-	{name: 'John C. Stormont', department: 'CE' },
-	{name: 'Susan Bogus', department: 'CE' },
-	{name: 'Julie E.A. Coonrod', department: 'CE' },
-	{name: 'Walter Gerstle', department: 'CE' },
-	{name: 'Gregory Gould', department: 'CE' },
-	{name: 'Jerome W. Hall', department: 'CE' },
-	{name: 'Kerry J. Howe', department: 'CE' },
-	{name: 'Arup K. Maji', department: 'CE' },
-	{name: 'Percy Ng', department: 'CE' },
-	{name: 'Timothy J. Ross', department: 'CE' },
-	{name: 'Jerald L. Rounds', department: 'CE' },
-	{name: 'Mark Russell', department: 'CE' },
-	{name: 'Andrew Schuler', department: 'CE' },
-	{name: 'Mark Stone', department: 'CE' },
-	{name: 'Mahmoud Reda Taha', department: 'CE' },
-	{name: 'Rafiqul A. Tarefder', department: 'CE' },
-	{name: 'Bruce M. Thomson', department: 'CE' },
-	{name: 'Vanessa Valentin', department: 'CE' },
-	{name: 'Guohui Zhang', department: 'CE' },    
-	
-	{name: 'Ed Arthur', department: 'CNE' },
-	{name: 'Plamen Atanassov', department: 'CNE' },
-	{name: 'Edward D. Blandford', department: 'CNE' },
-	{name: 'C. Jeffrey Brinker', department: 'CNE' },
-	{name: 'Robert Busch', department: 'CNE' },
-	{name: 'Heather Canavan', department: 'CNE' },
-	{name: 'Eric Carnes', department: 'CNE' },
-	{name: 'Joseph Cecchi', department: 'CNE' },
-	{name: 'Eva Chi', department: 'CNE' },
-	{name: 'Gary Cooper', department: 'CNE' },
-	{name: 'Abhaya Datye', department: 'CNE' },
-	{name: 'Cassiano de Oliveira', department: 'CNE' },
-	{name: 'Elizabeth Dirk', department: 'CNE' },
-	{name: 'Jeremy Edwards', department: 'CNE' },
-	{name: 'Mohamed El-Genk', department: 'CNE' },
-	{name: 'James Freyer', department: 'CNE' },
-	{name: 'Julia Fulghum', department: 'CNE' },
-	{name: 'Steve Graves', department: 'CNE' },
-	{name: 'Sang Eon Han', department: 'CNE' },
-	{name: 'Sang Han', department: 'CNE' },
-	{name: 'Adam Hecht', department: 'CNE' },
-	{name: 'Phillip H. Heintz', department: 'CNE' },
-	{name: 'Ronald Loehman', department: 'CNE' },
-	{name: 'Gabriel Lopez', department: 'CNE' },
-	{name: 'Patrick McDaniel', department: 'CNE' },
-	{name: 'Dimiter Petsev', department: 'CNE' },
-	{name: 'Anil Prinja', department: 'CNE' },
-	{name: 'Norman Roderick', department: 'CNE' },
-	{name: 'Randall Schunk', department: 'CNE' },
-	{name: 'Andrew Shreve', department: 'CNE' },
-	{name: 'Scott Sibbett', department: 'CNE' },
-	{name: 'Jean-Michel Tournier', department: 'CNE' },
-	{name: 'Tim L. Ward', department: 'CNE' },
-	{name: 'David Whitten', department: 'CNE' },
-	{name: 'Yixiang Xie', department: 'CNE' },
->>>>>>> da8da91c8a72757a7f881bdacb27eb24f018a9bf
+{name: 'Ed Arthur', department: 'CNE' },
+{name: 'Plamen Atanassov', department: 'CNE' },
+{name: 'Edward D. Blandford', department: 'CNE' },
+{name: 'C. Jeffrey Brinker', department: 'CNE' },
+{name: 'Robert Busch', department: 'CNE' },
+{name: 'Heather Canavan', department: 'CNE' },
+{name: 'Eric Carnes', department: 'CNE' },
+{name: 'Joseph Cecchi', department: 'CNE' },
+{name: 'Eva Chi', department: 'CNE' },
+{name: 'Gary Cooper', department: 'CNE' },
+{name: 'Abhaya Datye', department: 'CNE' },
+{name: 'Cassiano de Oliveira', department: 'CNE' },
+{name: 'Elizabeth Dirk', department: 'CNE' },
+{name: 'Jeremy Edwards', department: 'CNE' },
+{name: 'Mohamed El-Genk', department: 'CNE' },
+{name: 'James Freyer', department: 'CNE' },
+{name: 'Julia Fulghum', department: 'CNE' },
+{name: 'Steve Graves', department: 'CNE' },
+{name: 'Sang Eon Han', department: 'CNE' },
+{name: 'Sang Han', department: 'CNE' },
+{name: 'Adam Hecht', department: 'CNE' },
+{name: 'Phillip H. Heintz', department: 'CNE' },
+{name: 'Ronald Loehman', department: 'CNE' },
+{name: 'Gabriel Lopez', department: 'CNE' },
+{name: 'Patrick McDaniel', department: 'CNE' },
+{name: 'Dimiter Petsev', department: 'CNE' },
+{name: 'Anil Prinja', department: 'CNE' },
+{name: 'Norman Roderick', department: 'CNE' },
+{name: 'Randall Schunk', department: 'CNE' },
+{name: 'Andrew Shreve', department: 'CNE' },
+{name: 'Scott Sibbett', department: 'CNE' },
+{name: 'Jean-Michel Tournier', department: 'CNE' },
+{name: 'Tim L. Ward', department: 'CNE' },
+{name: 'David Whitten', department: 'CNE' },
+{name: 'Yixiang Xie', department: 'CNE' },
   ]
 professor_refs = {}
 
@@ -234,16 +136,16 @@ course_refs = {}
 
 
 evaluations = [
-    {name:       'Beginning of Course Questionnaire',
-     course:     'ECE-595-04-FALL-2012',
+    {name: 'Beginning of Course Questionnaire',
+     course: 'ECE-595-04-FALL-2012',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING'
     },
-    {name:       'Mid-course Evaluation',
-     course:     'ECE-595-04-FALL-2012',
+    {name: 'Mid-course Evaluation',
+     course: 'ECE-595-04-FALL-2012',
      unique_key: 'ECE-595-04-FALL-2012-MID'
     },
-    {name:       'End of Semester Evaluation',
-     course:     'ECE-595-04-FALL-2012',
+    {name: 'End of Semester Evaluation',
+     course: 'ECE-595-04-FALL-2012',
      unique_key: 'ECE-595-04-FALL-2012-END'
     },
   ]
@@ -251,35 +153,35 @@ evaluation_refs = {}
 
 
 questions = [
-    {content:    'Do you have any experience developing web applications?',
+    {content: 'Do you have any experience developing web applications?',
      evaluation: 'ECE-595-04-FALL-2012-BEGINNING',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q1'
     },
-    {content:    'What do you hope to get out of this class?',
+    {content: 'What do you hope to get out of this class?',
      evaluation: 'ECE-595-04-FALL-2012-BEGINNING',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q2'
     },
 
 
-    {content:    'How do you think the class is going?',
+    {content: 'How do you think the class is going?',
      evaluation: 'ECE-595-04-FALL-2012-MID',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q1'
     },
-    {content:    'How would you rate the pace of the course?',
+    {content: 'How would you rate the pace of the course?',
      evaluation: 'ECE-595-04-FALL-2012-MID',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q2'
     },
-    {content:    'Would you rather have a group project or more homeworks?',
+    {content: 'Would you rather have a group project or more homeworks?',
      evaluation: 'ECE-595-04-FALL-2012-MID',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q3'
     },
 
 
-    {content:    'Overall, how would you rate this course?',
+    {content: 'Overall, how would you rate this course?',
      evaluation: 'ECE-595-04-FALL-2012-END',
      unique_key: 'ECE-595-04-FALL-2012-END-Q1'
     },
-    {content:    'Have you achieved your goals for the class?',
+    {content: 'Have you achieved your goals for the class?',
      evaluation: 'ECE-595-04-FALL-2012-END',
      unique_key: 'ECE-595-04-FALL-2012-END-Q2'
     },
@@ -290,104 +192,104 @@ question_refs = {}
 answers = [
     # BEGINNING
     # Q1
-    {content:    'Yes',
-     question:   'ECE-595-04-FALL-2012-BEGINNING-Q1',
+    {content: 'Yes',
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q1',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q1-A1'
     },
-    {content:    'No',
-     question:   'ECE-595-04-FALL-2012-BEGINNING-Q1',
+    {content: 'No',
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q1',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q1-A2'
     },
 
     # Q2
-    {content:    'General background in developing web applications',
-     question:   'ECE-595-04-FALL-2012-BEGINNING-Q2',
+    {content: 'General background in developing web applications',
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q2',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q2-A1'
     },
-    {content:    'Some hands-on experience developing web applications',
-     question:   'ECE-595-04-FALL-2012-BEGINNING-Q2',
+    {content: 'Some hands-on experience developing web applications',
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q2',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q2-A2'
     },
-    {content:    'Die-hard experience developing complex web applications',
-     question:   'ECE-595-04-FALL-2012-BEGINNING-Q2',
+    {content: 'Die-hard experience developing complex web applications',
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q2',
      unique_key: 'ECE-595-04-FALL-2012-BEGINNING-Q2-A3'
     },
 
 
     # Mid-semester
     # Q1
-    {content:    'Great',
-     question:   'ECE-595-04-FALL-2012-MID-Q1',
+    {content: 'Great',
+     question: 'ECE-595-04-FALL-2012-MID-Q1',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q1-A1'
     },
-    {content:    'Pretty good',
-     question:   'ECE-595-04-FALL-2012-MID-Q1',
+    {content: 'Pretty good',
+     question: 'ECE-595-04-FALL-2012-MID-Q1',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q1-A2'
     },
-    {content:    'Not so good',
-     question:   'ECE-595-04-FALL-2012-MID-Q1',
+    {content: 'Not so good',
+     question: 'ECE-595-04-FALL-2012-MID-Q1',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q1-A3'
     },
-    {content:    'Horribly',
-     question:   'ECE-595-04-FALL-2012-MID-Q1',
+    {content: 'Horribly',
+     question: 'ECE-595-04-FALL-2012-MID-Q1',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q1-A4'
     },
 
     # Q2
-    {content:    'Too fast',
-     question:   'ECE-595-04-FALL-2012-MID-Q2',
+    {content: 'Too fast',
+     question: 'ECE-595-04-FALL-2012-MID-Q2',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q2-A1'
     },
-    {content:    'About right',
-     question:   'ECE-595-04-FALL-2012-MID-Q2',
+    {content: 'About right',
+     question: 'ECE-595-04-FALL-2012-MID-Q2',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q2-A2'
     },
-    {content:    'Too slow',
-     question:   'ECE-595-04-FALL-2012-MID-Q2',
+    {content: 'Too slow',
+     question: 'ECE-595-04-FALL-2012-MID-Q2',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q2-A3'
     },
 
     # Q3
-    {content:    'Group project',
-     question:   'ECE-595-04-FALL-2012-MID-Q3',
+    {content: 'Group project',
+     question: 'ECE-595-04-FALL-2012-MID-Q3',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q3-A1'
     },
-    {content:    'More homework',
-     question:   'ECE-595-04-FALL-2012-MID-Q3',
+    {content: 'More homework',
+     question: 'ECE-595-04-FALL-2012-MID-Q3',
      unique_key: 'ECE-595-04-FALL-2012-MID-Q3-A2'
     },
 
 
     # End of semester
     # Q1
-    {content:    'Excellent',
-     question:   'ECE-595-04-FALL-2012-END-Q1',
+    {content: 'Excellent',
+     question: 'ECE-595-04-FALL-2012-END-Q1',
      unique_key: 'ECE-595-04-FALL-2012-END-Q1-A1'
     },
-    {content:    'Good',
-     question:   'ECE-595-04-FALL-2012-END-Q1',
+    {content: 'Good',
+     question: 'ECE-595-04-FALL-2012-END-Q1',
      unique_key: 'ECE-595-04-FALL-2012-END-Q1-A2'
     },
-    {content:    'Average',
-     question:   'ECE-595-04-FALL-2012-END-Q1',
+    {content: 'Average',
+     question: 'ECE-595-04-FALL-2012-END-Q1',
      unique_key: 'ECE-595-04-FALL-2012-END-Q1-A3'
     },
-    {content:    'Poor',
-     question:   'ECE-595-04-FALL-2012-END-Q1',
+    {content: 'Poor',
+     question: 'ECE-595-04-FALL-2012-END-Q1',
      unique_key: 'ECE-595-04-FALL-2012-END-Q1-A4'
     },
-    {content:    'Terrible',
-     question:   'ECE-595-04-FALL-2012-END-Q1',
+    {content: 'Terrible',
+     question: 'ECE-595-04-FALL-2012-END-Q1',
      unique_key: 'ECE-595-04-FALL-2012-END-Q1-A5'
     },
 
     # Q2
-    {content:    'Yes',
-     question:   'ECE-595-04-FALL-2012-END-Q2',
+    {content: 'Yes',
+     question: 'ECE-595-04-FALL-2012-END-Q2',
      unique_key: 'ECE-595-04-FALL-2012-END-Q2-A1'
     },
-    {content:    'No',
-     question:   'ECE-595-04-FALL-2012-END-Q2',
+    {content: 'No',
+     question: 'ECE-595-04-FALL-2012-END-Q2',
      unique_key: 'ECE-595-04-FALL-2012-END-Q2-A2'
     },
   ]
@@ -410,30 +312,30 @@ response_set_refs = {}
 
 responses = [
     {response_set: 'ECE-595-04-FALL-2012-BEGINNING-RS1',
-     question:     'ECE-595-04-FALL-2012-BEGINNING-Q1',
-     answer:       'ECE-595-04-FALL-2012-BEGINNING-Q1-A1'
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q1',
+     answer: 'ECE-595-04-FALL-2012-BEGINNING-Q1-A1'
     },
     {response_set: 'ECE-595-04-FALL-2012-BEGINNING-RS1',
-     question:     'ECE-595-04-FALL-2012-BEGINNING-Q2',
-     answer:       'ECE-595-04-FALL-2012-BEGINNING-Q2-A1'
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q2',
+     answer: 'ECE-595-04-FALL-2012-BEGINNING-Q2-A1'
     },
 
     {response_set: 'ECE-595-04-FALL-2012-BEGINNING-RS2',
-     question:     'ECE-595-04-FALL-2012-BEGINNING-Q1',
-     answer:       'ECE-595-04-FALL-2012-BEGINNING-Q1-A2'
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q1',
+     answer: 'ECE-595-04-FALL-2012-BEGINNING-Q1-A2'
     },
     {response_set: 'ECE-595-04-FALL-2012-BEGINNING-RS2',
-     question:     'ECE-595-04-FALL-2012-BEGINNING-Q2',
-     answer:       'ECE-595-04-FALL-2012-BEGINNING-Q2-A2'
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q2',
+     answer: 'ECE-595-04-FALL-2012-BEGINNING-Q2-A2'
     },
 
     {response_set: 'ECE-595-04-FALL-2012-BEGINNING-RS3',
-     question:     'ECE-595-04-FALL-2012-BEGINNING-Q1',
-     answer:       'ECE-595-04-FALL-2012-BEGINNING-Q1-A2'
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q1',
+     answer: 'ECE-595-04-FALL-2012-BEGINNING-Q1-A2'
     },
     {response_set: 'ECE-595-04-FALL-2012-BEGINNING-RS3',
-     question:     'ECE-595-04-FALL-2012-BEGINNING-Q2',
-     answer:       'ECE-595-04-FALL-2012-BEGINNING-Q2-A3'
+     question: 'ECE-595-04-FALL-2012-BEGINNING-Q2',
+     answer: 'ECE-595-04-FALL-2012-BEGINNING-Q2-A3'
     },
   ]
 
@@ -461,7 +363,7 @@ end
 Course.delete_all
 courses.each do |course|
   department = department_refs[course[:department]]
-  professor  = professor_refs[course[:professor]]
+  professor = professor_refs[course[:professor]]
   course_key = course[:unique_key]
   course.delete :department
   course.delete :professor
@@ -523,8 +425,8 @@ end
 Response.delete_all
 responses.each do |response|
   response_set = response_set_refs[response[:response_set]]
-  question     = question_refs[response[:question]]
-  answer       = answer_refs[response[:answer]]
+  question = question_refs[response[:question]]
+  answer = answer_refs[response[:answer]]
   response.delete :response_set
   response.delete :question
   response.delete :answer
@@ -532,3 +434,6 @@ responses.each do |response|
   response[:answer_id] = answer.id
   response_set.responses.create response
 end
+
+
+
